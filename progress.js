39d9863409275ceadf0dd77f9072ba01249cc328
structArca <structarca@gmail.com>
2025-03-02 03:43:54 +0900

    0.00

---
+++ progress.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOMContentLoaded イベントが発生しました");
+    // テキストファイルのパス
+    const filePath = 'D:\\data\\Aruca=CIAN\\Arca\\github\\website\\data.txt';
+
+    // Fetch APIを使用してテキストファイルを読み込む
+    fetch(filePath)
+        .then(response => response.text())
+        .then(text => {
+            // テキストファイルの内容を表示する
+            document.getElementById('content').textContent = text;
+        })
+        .catch(error => console.error('Error:', error));
+});
