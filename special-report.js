b6f865605dfcd3324055713256a203cc39ee855b
structArca <structarca@gmail.com>
2025-03-08 05:49:05 +0900

    0.05

--- special-report.js
+++ special-report.js
@@ -23,7 +23,7 @@ function DrawSpecialReport(resourceText)
     const src = String(resourceText);
 
     document.getElementById(contentIDSave).innerHTML = "読み込み中...";
-    var dest = "";
+    var dest = "<div style = \"display: flex;\">";
 
     for(i = 0, j = 0, n = 0, iStart = 0; i < src.length; i++){
         if(src[i] == ',' || src[i] == '\n'){
@@ -36,34 +36,70 @@ function DrawSpecialReport(resourceText)
         }
 
         if(src[i] == '\n'){
-            n++;
-            iStart = i+1;
+
+            dest += "<div style = \"\
+                position: relative;\
+                margin: 0;\
+                margin-right: 2vh;\
+                padding: 0;\
+            \">";
 
             if(data[1] == ""){
                 data[1] = "無題"
             }
-            dest += "<h4>" + data[1] + "</h4>";
+
+            dest += "<h4 style = '\
+                overflow: visible;\
+            '>" + data[1] + "</h4>";
             if(data[3] == ""){
             }else{
                 dest += "<p>" + data[3] + "</p>";
             }
             if(data[2] == ""){
             }else{
-                dest += "<iframe src = \"" + data[2] + "\" name = \"" + data[1] + "\" >" + data[2] + "</iframe><br>";
+                dest += "<a href = '" + data[2] + "' target = '" + data[1] + "' ";
+                dest += "style = '\
+                    position: relative;\
+                    margin: 0;\
+                    padding-left: 2vw;\
+                    padding-right: 2vw;\
+                    padding-top: 2vh;\
+                    padding-bottom: 2vh;\
+                    width: 8vw;\
+                    height: 6vh;\
+                    font-size: 1vw;\
+                    text-align: center;\
+                    background-color: #333333;\
+                    color: #FFFFFF;'\
+                    >"
+                dest += "No Image.";
+                dest += "<img src = \"" + data[2] + "\" onError = 'this.style.display = \"none\"'\>";
+                dest += "</a>";
             }
             if(data[0] == ""){
             }else{
-                dest += "<small> " + data[0] + "</small>";
+                dest += "<div style = '\
+                position: relative;\
+                margin: 0;\
+                padding: 0;\
+                top: 2vh;\
+                text-size: 0.5vw;\
+                opacity: 30%;\
+                '>" + data[0] + "</div>";
             }
-            dest += "<br><br>";
+            // dest += "<br><br>";
+            dest += "</div>";
 
             for(j = 0; j < dataLength; j++){
                 data[j] = "";
             }
+            iStart = i+1;
             j = 0;
+            n++;
         }
     }
 
+    dest += "<div><br><br>";
     document.getElementById(contentIDSave).innerHTML = dest;
 };
 
